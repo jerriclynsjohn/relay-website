953feaab3c0cdf231bd4c9886add669dedae8805
Jerric Lyns John <jerriclynsjohn@outlook.com>
2024-10-15 13:42:43 +0530

    feat: initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import mdx from '@astrojs/mdx';
+
+import tailwind from '@astrojs/tailwind';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), mdx(), tailwind(), sitemap()]
+});
